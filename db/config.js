0719c2489c171cca2115fb4b1d208459d3c3c246
svladiko <vlad.serhiychuk@gmail.com>
2023-01-11 17:52:29 +0200

    Changed connection stirng

--- db/config.js
+++ db/config.js
@@ -1,7 +1,7 @@
 const { tls } = require('node-tls');
 
 const config = {
-         connectionString: 'postgres://bpflvidbquvpdp:a7664b3b120bba81df36a108a98765925157f901cec8f0228f52533004ac96b3@ec2-63-32-248-14.eu-west-1.compute.amazonaws.com:5432/d6j0lksqcelm8h',
+         connectionString: 'postgres://ygfgtyshodgjnv:8a42b867639775284cb4ec046e204172f9b921800eb40e6105117e9764987882@ec2-52-48-159-67.eu-west-1.compute.amazonaws.com:5432/d2klbg8sa9s7gl',
          ssl: {
          rejectUnauthorized: false
      }
