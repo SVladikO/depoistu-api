b77978c9fb0d68f7ec1c3ca25a42bb7d4ef66358
svladiko <vlad.serhiychuk@gmail.com>
2023-01-12 22:48:41 +0200

    Improved log to terminal.

--- index.js
+++ index.js
@@ -2,31 +2,65 @@ const express = require('express')
 const cors = require('cors');
 const open = require('open'); // open browser after run
 const QUERY = require('./db/query')
-const dbRequest = require('./utils')
+const {dbRequest, getParamMessageRequirements, logRequestDetails} = require('./utils')
 const server = express();
 server.use(cors());
 
 server.get('/company/:companyId/menu', function (req, res) {
+    logRequestDetails(req)
+
     const {companyId} = req.params;
-    dbRequest(QUERY.MENU_ITEM.SELECT_ALL_BY_COMPANY_ID(companyId), value => res.send(value));
+
+    if (isNaN(companyId)) {
+        res.send(getParamMessageRequirements('companyId',));
+        return;
+    }
+
+    dbRequest(QUERY.MENU_ITEM.SELECT_ALL_BY_COMPANY_ID(companyId), dbResponce => res.send(dbResponce));
 })
 
 server.get('/company/:companyId/category', function (req, res) {
+    logRequestDetails(req)
     const {companyId} = req.params;
+
+    if (isNaN(companyId)) {
+        res.send(getParamMessageRequirements('companyId'))
+        return;
+    }
+
     dbRequest(
         QUERY.MENU_ITEM.SELECT_CATEGORY_ID_BY_COMPANY_ID(companyId),
-        value => {
-            let result = [...new Set(value.map(v => v.category_id))]
+        dbResponce => {
+            let result = [...new Set(dbResponce.map(v => v.category_id))]
             res.send(result)
-        });
+        },
+        dbResponce => res.send(dbResponce)
+    );
 })
 
-server.get('/company/:companyId/menu/:categoryId', function (req, res) {
+const URL = {
+
+    MENU_ITEM: '/company/:companyId/menu_item/:categoryId',
+}
+
+server.get(URL.MENU_ITEM, function (req, res) {
+    logRequestDetails(req)
     const {companyId, categoryId} = req.params;
+
+    if (isNaN(companyId)) {
+        res.send(getParamMessageRequirements('companyId'))
+        return;
+    }
+
+    if (isNaN(categoryId)) {
+        res.send(getParamMessageRequirements('categoryId'))
+        return;
+    }
 
     dbRequest(
         QUERY.MENU_ITEM.SELECT_ALL_BY_COMPANY_ID_AND_BY_CATEGORY_ID(companyId, categoryId),
-        value => res.send(value)
+        dbResponce => res.send(dbResponce),
+        dbResponce => res.send(dbResponce)
     );
 })
 
--- utils.js
+++ utils.js
@@ -3,13 +3,15 @@ const dbConfig = require("./db/config");
 
 const pool = new Pool(dbConfig);
 
-function dbRequest(dbRequest, onSuccess, onError) {
+function dbRequest(dbRequest, onSuccess = () => {
+}, onError = () => {
+}) {
     console.log('!!!! NEW DB REQUEST !!!!', dbRequest)
 
     pool.connect().then(client => {
         client.query(dbRequest)
             .then(res => {
-                console.log('<<<<<<< DB REQUEST SUCCESS >>>>>>>')
+                console.log('!!!! DB REQUEST SUCCESS !!!!')
                 console.log('DB RESPONSE: ', res.rows)
 
                 client.release()
@@ -18,28 +20,31 @@ function dbRequest(dbRequest, onSuccess, onError) {
             })
             .catch(e => {
 
-                console.log('??????? DB REQUEST ERROR ??????????')
-                console.log('??????? MESSAGE: ', e.message);
-                console.log('??????? STACK: ', e.stack);
+                console.log('???? DB REQUEST ERROR ????')
+                console.log('???? MESSAGE: ', e.message);
+                console.log('???? STACK: ', e.stack);
 
                 client.release()
                 onError('DB Error:' + e.message)
             })
     })
-
-
-
-    // pool.connect()
-    // pool.query(dbRequest)
-    //     .then(r => {
-    //         console.log('DB request: ', dbRequest)
-    //         console.log('DB response: ', r.rows)
-    //         onSuccess(r.rows)
-    //     })
-    //     .catch(e => {
-    //         console.error('DB error: ', e.stack)
-    //         onError('DB error: ' + e.stack)
-    //     })
 }
 
-module.exports = dbRequest;
+const getParamMessageRequirements = (paramName, requiredType= 'number') => {
+    const message = `Error: Param ${paramName} should be ${requiredType}`;
+    console.log('???? ' + message)
+    return message;
+}
+
+const logRequestDetails = req => {
+    console.log('>>>> New request <<<<');
+    console.log(req._parsedUrl.pathname);
+    console.log(req.route.path);
+    console.log('Method: ', req.route.stack[0].method);
+}
+
+module.exports = {
+    getParamMessageRequirements,
+    dbRequest,
+    logRequestDetails,
+};
