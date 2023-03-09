ac145884b79c78f06026714d260e0e6109ee72f4
Vlad Serhiychuk <vlad.serhiychuk@gmail.com>
2023-03-09 19:44:23 +0200

    [PMA-179] Added post create company

--- route/company.js
+++ route/company.js
@@ -5,6 +5,7 @@ const routes = {
     "name": "Company",
     "description": "For company data.",
     "routes": [
+
         {
             "method": "get",
             "url": "/companies",
@@ -16,12 +17,25 @@ const routes = {
         },
         {
             "method": "get",
-            "url": "/companies/:city",
+            "url": "/companies/?x=:city",
             "description": "Get companies by city. Only Ukrainian for now. Case sensitive.",
             callback: function (req, res) {
                 logRequestDetails(req)
                 const {city} = req.params;
-                dbRequest(QUERY.COMPANY.SELECT_BY_CITY(city), dbRes => res.send(dbRes));
+                dbRequest(QUERY.COMPANY.SELECT_BY_CITY(city), dbRes => res.send(dbRes), message => res.send(message));
+            }
+        },
+        {
+            "method": "post",
+            "url": "/companies",
+            "description": "Create company.",
+            callback: function (req, res) {
+                logRequestDetails(req);
+                const {name, phones, email, city, street, schedule, photos} = req.body;
+                const join_date = '' + new Date().getTime()
+                console.log(1111, name, phones, email, city, street, join_date, schedule, photos)
+                const company = {name, phones, email, city, street, join_date, schedule, photos};
+                dbRequest(QUERY.COMPANY.INSERT(company), dbRes => res.send(dbRes), message => res.send(message));
             }
         }
     ]
