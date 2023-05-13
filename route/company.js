56bc7689241527882e607690c3600d02a1850118
Vlad Serhiychuk <SVladikO@mail.ru>
2023-05-13 19:33:48 +0300

    [PMA-334] Delete extra routs.

--- route/company.js
+++ route/company.js
@@ -7,13 +7,31 @@ const routes = {
     "name": "Company",
     "description": "For company data.",
     "routes": [
-
+        // {
+        //     "method": "get",
+        //     "url": "/companies",
+        //     "description": "Get all companies.",
+        //     callback: function (req, res) {
+        //         dbRequest(QUERY.COMPANY.SELECT_ALL(),
+        //             dbRes => res.send(dbRes),
+        //             errorMessage => res.send(errorMessage)
+        //         );
+        //     }
+        // },
         {
             "method": "get",
-            "url": "/companies",
-            "description": "Get all companies.",
+            "url": "/companies/by/city/:city",
+            "description": "Get companies by city. Only Ukrainian for now. Case sensitive.",
             callback: function (req, res) {
-                dbRequest(QUERY.COMPANY.SELECT_ALL(),
+                const {city} = req.params;
+
+                if (city === 'undefined' || Number.isInteger(+city)) {
+                    return res.status(400).send({
+                        message: 'Bad request.'
+                    })
+                }
+
+                dbRequest(QUERY.COMPANY.SELECT_BY_CITY(city),
                     dbRes => res.send(dbRes),
                     errorMessage => res.send(errorMessage)
                 );
@@ -40,34 +58,6 @@ const routes = {
         },
         {
             "method": "get",
-            "url": "/companies/by/id/:companyId",
-            "description": "Get company by companyId.",
-            callback: function (req, res) {
-                const {companyId} = req.params;
-                dbRequest(QUERY.COMPANY.SELECT_BY_COMPANY_ID(companyId), dbRes => res.send(dbRes), message => res.send(message));
-            }
-        },
-        {
-            "method": "get",
-            "url": "/companies/by/city/:city",
-            "description": "Get companies by city. Only Ukrainian for now. Case sensitive.",
-            callback: function (req, res) {
-                const {city} = req.params;
-
-                if (city === 'undefined' || Number.isInteger(+city)) {
-                    return res.status(400).send({
-                        message: 'Bad request.'
-                    })
-                }
-
-                dbRequest(QUERY.COMPANY.SELECT_BY_CITY(city),
-                    dbRes => res.send(dbRes),
-                    errorMessage => res.send(errorMessage)
-                );
-            }
-        },
-        {
-            "method": "get",
             "url": "/companies/by/customer/:customerId",
             "description": "Get companies by customer id.",
             callback: function (req, res) {
@@ -83,15 +73,6 @@ const routes = {
                     dbRes => res.send(dbRes),
                     errorMessage => res.send(errorMessage)
                 );
-            }
-        },
-        {
-            "method": "get",
-            "url": "/companies/by/customer/:customerId",
-            "description": "Get companies by customer id.",
-            callback: function (req, res) {
-                const {customerId} = req.params;
-                dbRequest(QUERY.COMPANY.SELECT_BY_CUSTOMER_ID(customerId), dbRes => res.send(dbRes), message => res.send(message));
             }
         },
         {
@@ -116,7 +97,7 @@ const routes = {
                         responseError(res, 400, e.message);
                     })
             }
-        }
+        },
     ]
 };
 
--- route/menu_item.js
+++ route/menu_item.js
@@ -28,88 +28,45 @@ const routes = {
                     errorMessage => res.send(errorMessage));
             }
         },
-        {
-            "method": "get",
-            "url": "/menu/categories/:companyId",
-            "description": "Get menu categories by company id.",
-            callback: function (req, res) {
-                const {companyId} = req.params;
-
-                if (isNaN(companyId)) {
-                    res.send(getParamMessageRequirements('companyId'))
-                    return;
-                }
-
-                dbRequest(
-                    QUERY.MENU_ITEM.SELECT_CATEGORY_ID_BY_COMPANY_ID(companyId),
-                    dbRes => {
-                        let result = [...new Set(dbRes.map(v => v.category_id))]
-                        res.send(result)
-                    },
-                    errorMessage => res.send(errorMessage)
-                );
-            }
-        },
-        {
-            "method": "get",
-            "url": "/menu/:companyId/:categoryId",
-            "description": "Get menu for specific company and category.",
-            callback: function (req, res) {
-                const {companyId, categoryId} = req.params;
-
-                if (isNaN(companyId)) {
-                    res.send(getParamMessageRequirements('companyId'))
-                    return;
-                }
-
-                if (isNaN(categoryId)) {
-                    res.send(getParamMessageRequirements('categoryId'))
-                    return;
-                }
-
-                dbRequest(
-                    QUERY.MENU_ITEM.SELECT_ALL_BY_COMPANY_ID_AND_BY_CATEGORY_ID(companyId, categoryId),
-                    dbRes => res.send(dbRes),
-                    errorMessage => res.send(errorMessage)
-                );
-            }
-        },
-        {
-            "method": "post",
-            "url": "/menu",
-            "description": "Create menu item.",
-            callback: function (req, res) {
-                const {category_id, company_id, name, description, cooking_time, price, size, image_url} = req.body;
-
-                dbRequest(
-                    QUERY.MENU_ITEM.INSERT(category_id, company_id, name, description, cooking_time, price, size, image_url),
-                    dbRes => res.send(dbRes),
-                    errorMessage => res.send(errorMessage)
-                );
-            }
-        },
-        {
-            "method": "post",
-            "url": "/menu",
-            "description": "Create menu item.",
-            callback: function (req, res) {
-                const {category_id, company_id, name, description, cooking_time, price, size, image_url} = req.body;
-
-                dbRequest(
-                    QUERY.MENU_ITEM.INSERT(category_id, company_id, name, description, cooking_time, price, size, image_url),
-                        dbRes => res.send(dbRes), message => res.send(message));
-            }
         // {
-        //     "category_id": 1,
-        //     "company_id": 111,
-        //     "name": "Kasha",
-        //     "description": "the best kasha ever",
-        //     "cooking_time": "10",
-        //     "price": "100",
-        //     "size": "110",
-        //     "image_url": "https://upload.wikimedia.org/wikipedia/commons/thumb/6/66/Havregr%C3%B8d_p%C3%A5_vand.JPG/280px-Havregr%C3%B8d_p%C3%A5_vand.JPG"
-        // }
-        }
+        //     "method": "get",
+        //     "url": "/menu/:companyId/:categoryId",
+        //     "description": "Get menu for specific company and category.",
+        //     callback: function (req, res) {
+        //         const {companyId, categoryId} = req.params;
+        //
+        //         if (isNaN(companyId)) {
+        //             res.send(getParamMessageRequirements('companyId'))
+        //             return;
+        //         }
+        //
+        //         if (isNaN(categoryId)) {
+        //             res.send(getParamMessageRequirements('categoryId'))
+        //             return;
+        //         }
+        //
+        //         dbRequest(
+        //             QUERY.MENU_ITEM.SELECT_ALL_BY_COMPANY_ID_AND_BY_CATEGORY_ID(companyId, categoryId),
+        //             dbRes => res.send(dbRes),
+        //             errorMessage => res.send(errorMessage)
+        //         );
+        //     }
+        // },
+        {
+            "method": "post",
+            "url": "/menu",
+            "description": "Create menu item.",
+            callback: function (req, res) {
+                const {category_id, company_id, name, description, cooking_time, price, size, image_url} = req.body;
+
+                dbRequest(
+                    QUERY.MENU_ITEM.INSERT(category_id, company_id, name, description, cooking_time, price, size, image_url),
+                    dbRes => res.send(dbRes),
+                    errorMessage => res.send(errorMessage)
+                );
+            }
+        },
+
     ]
 }
 
