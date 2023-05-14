5cc72ba669edd60088ef2ac8b6931fe6c290671b
svladiko <vlad.serhiychuk@gmail.com>
2023-05-14 16:45:45 +0300

    [PMA-335] Added DB server label

--- db/config.js
+++ db/config.js
@@ -1,9 +1,18 @@
 const config = {
-    host: 'mysql-113709-0.cloudclusters.net',
-    user: 'admin',
-    password: 'rzYPgxWa',
-    database: 'root',
-    port: 18036
+    master: {
+        host: 'mysql-126266-0.cloudclusters.net',
+        user: 'admin',
+        password: 'y3QV0azM',
+        database: 'root',
+        port: 18854
+    },
+    develop: {
+        host: 'mysql-113709-0.cloudclusters.net',
+        user: 'admin',
+        password: 'rzYPgxWa',
+        database: 'root',
+        port: 18036
+    }
 };
 
 module.exports = config;
