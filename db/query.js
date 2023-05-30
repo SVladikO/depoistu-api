567ee9145523752647d07a04d67043f69b935232
Vlad Serhiychuk <vlad.serhiychuk@gmail.com>
2023-05-30 18:31:21 +0300

    [PMA-309] Added POST menu

--- db/query.js
+++ db/query.js
@@ -6,14 +6,11 @@ const QUERY = {
                 companyId =>  `select category_id from MENU_ITEM where company_id = ${companyId};`,
         SELECT_ALL_BY_COMPANY_ID_AND_BY_CATEGORY_ID:
             (companyId, categoryId) =>  `select * from MENU_ITEM where category_id = ${categoryId} and company_id = ${companyId};`,
-        INSERT:
-            (category_id, company_id, name, description, cooking_time, price, size, image_url) => `
-              INSERT INTO MENU_ITEM (id, category_id, company_id, name, description, cooking_time, price, size, image_url) VALUES
-              (default, '${category_id}', '${company_id}', '${name}', '${description}', '${cooking_time}', '${price}', '${size}', '${image_url}');`
+        INSERT: mi => `INSERT INTO MENU_ITEM (id, category_id, company_id, name, description, cooking_time, price, size, image_url) VALUES
+              (default, '${mi.category_id}', '${mi.company_id}', '${mi.name}', '${mi.description}', '${mi.cookingTime}', '${mi.price}', '${mi.size}', '${mi.image_url}');`
     },
     HISTORY: {
-        INSERT:
-            (customer_id, company_id, order_details, date_time) =>
+        INSERT: (customer_id, company_id, order_details, date_time) =>
                 `INSERT INTO HISTORY (id, CUSTOMER_ID, COMPANY_ID, ORDER_DETAILS, DATE_TIME, is_paid, is_prepared) VALUES
                  (DEFAULT, ${customer_id}, ${company_id}, '${order_details}', ${date_time}, true, true);`
     },
@@ -21,10 +18,8 @@ const QUERY = {
         SELECT_BY_CUSTOMER_ID: customerId => `SELECT * from COMPANY WHERE customer_id = '${customerId}';`,
         SELECT_BY_CITY: city => `SELECT * from COMPANY WHERE city = '${city}';`,
         SELECT_BY_COMPANY_ID: companyId => `SELECT *from COMPANY WHERE id = '${companyId}';`,
-        SELECT_ALL:
-            () => `SELECT * FROM COMPANY;`,
-        INSERT:
-                c => `INSERT INTO COMPANY (id, customer_id, name, phone, city, street, join_date, schedule) VALUES
+        SELECT_ALL: () => `SELECT * FROM COMPANY;`,
+        INSERT: c => `INSERT INTO COMPANY (id, customer_id, name, phone, city, street, join_date, schedule) VALUES
                       (
                         DEFAULT, 
                         '${c.customer_id}', 
