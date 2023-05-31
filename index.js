c912444d428682e713eed25d5af80049b1033c05
Vlad Serhiychuk <vlad.serhiychuk@gmail.com>
2023-05-31 17:11:46 +0300

    [PMA-342] Fixed CORS.

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const routes = require('./route/index');
 
 const app = express();
 
-app.use(cors());
+
+app.use(cors(corsOptionsDelegate));
 app.use(express.json());
 app.use(express.static('public'));
 app.use(logRequestDetails);
@@ -29,3 +30,15 @@ app.listen(PORT, () => console.log(`app running on port ${PORT}`));
 open('http://localhost:' + PORT)
 
 module.exports = app;
+
+function corsOptionsDelegate(req, callback) {
+    const allowlist = ['https://pma-production.onrender.com', 'https://pma-develop.onrender.com', 'http://localhost:3000']
+    let corsOptions;
+
+    if (allowlist.indexOf(req.header('Origin')) !== -1) {
+        corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+    } else {
+        corsOptions = { origin: false } // disable CORS for this request
+    }
+    callback(null, corsOptions) // callback expects two parameters: error and options
+}
