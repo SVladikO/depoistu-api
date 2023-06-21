632557a48d18bfcfb3879a67d2aeb7573c0b31ed
svladiko <vlad.serhiychuk@gmail.com>
2023-06-21 18:40:14 +0300

    [PMA-410] Changed company get by city route description.

--- utils/description.js
+++ utils/description.js
@@ -5,7 +5,7 @@ const DESCRIPTION = {
         CHANGE_PASSWORD: "Customer change password.",
     },
     COMPANY: {
-        GET_BY_CITY: "Get companies by city. Only Ukrainian for now. Case sensitive.",
+        GET_BY_CITY: "Get companies by city uniq identifier.",
         GET_BY_COMPANY_ID: "Get company by companyId.",
         GET_BY_CUSTOMER_ID: "Get companies by customer id.",
         GET_AVAILABLE_CITIES: "Get array cities. Avoid duplication.",
