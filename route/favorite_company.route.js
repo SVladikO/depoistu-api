3f710f68e229dece5d64ad4cb7c6964196e7f4f1
v.serhiichuk <Vladyslav.Serhiichuk@playtech.com>
2023-11-19 00:10:33 +0200

    F

--- route/favorite_company.route.js
+++ route/favorite_company.route.js
@@ -5,6 +5,7 @@ const {DESCRIPTION} = require("../utils/description.utils");
 const {verifyToken} = require("../middleware/auth.middleware");
 const {convertCompanyFields} = require("../utils/company.utils")
 const {sendHandler, catchHandler} = require("../utils/handler.utils")
+const {Logger} = require("../middleware/log.middleware");
 
 const routes = {
     "name": "Favorite company",
@@ -18,14 +19,16 @@ const routes = {
             callbacks: [
                 verifyToken,
                 function (req, res) {
+                    const logger = new Logger(req);
+                    logger.addLog(DESCRIPTION.FAVORITE_COMPANY.GET_BY_CUSTOMER_ID)
 
-                const {id: customer_id } = req.customer;
+                    const {id: customer_id} = req.customer;
 
-                dbRequest(QUERY.FAVORITE_COMPANY.SELECT_BY_CUSTOMER_ID(customer_id))
-                    .then(convertCompanyFields)
-                    .then(sendHandler(res))
-                    .catch(catchHandler(res, DESCRIPTION.FAVORITE_COMPANY.GET_BY_CUSTOMER_ID, customer_id))
-            }]
+                    dbRequest(logger.addQueryDB(QUERY.FAVORITE_COMPANY.SELECT_BY_CUSTOMER_ID(customer_id)))
+                        .then(convertCompanyFields)
+                        .then(sendHandler(res, logger))
+                        .catch(catchHandler({res, logger, status: 400}));
+                }]
         },
         {
             method: "post",
@@ -35,14 +38,16 @@ const routes = {
             callbacks: [
                 verifyToken,
                 function (req, res) {
+                    const logger = new Logger(req);
+                    logger.addLog(DESCRIPTION.FAVORITE_COMPANY.ADD)
 
-                const {id: customer_id } = req.customer;
-                const {company_id } = req.body;
+                    const {id: customer_id} = req.customer;
+                    const {company_id} = req.body;
 
-                dbRequest(QUERY.FAVORITE_COMPANY.ADD(customer_id, company_id))
-                    .then(sendHandler(res))
-                    .catch(catchHandler(res, DESCRIPTION.FAVORITE_COMPANY.GET_BY_CUSTOMER_ID, customer_id))
-            }]
+                    dbRequest(logger.addQueryDB(QUERY.FAVORITE_COMPANY.ADD(customer_id, company_id)))
+                        .then(sendHandler(res, logger))
+                        .catch(catchHandler({res, logger, status: 400}));
+                }]
         },
         {
             method: "delete",
@@ -52,14 +57,16 @@ const routes = {
             callbacks: [
                 verifyToken,
                 function (req, res) {
+                    const logger = new Logger(req);
+                    logger.addLog(DESCRIPTION.FAVORITE_COMPANY.DELETE)
 
-                const {id: customer_id } = req.customer;
-                const {company_id } = req.body;
+                    const {id: customer_id} = req.customer;
+                    const {company_id} = req.body;
 
-                dbRequest(QUERY.FAVORITE_COMPANY.DELETE(customer_id, company_id))
-                    .then(sendHandler(res))
-                    .catch(catchHandler(res, DESCRIPTION.FAVORITE_COMPANY.GET_BY_CUSTOMER_ID, customer_id))
-            }]
+                    dbRequest(logger.addQueryDB(QUERY.FAVORITE_COMPANY.DELETE(customer_id, company_id)))
+                        .then(sendHandler(res, logger))
+                        .catch(catchHandler({res, logger, status: 400}));
+                }]
         },
     ]
 };
