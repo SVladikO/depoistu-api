d35ee4d9dfcb4f05a7652641e36e7b889e07e173
v.serhiichuk <Vladyslav.Serhiichuk@playtech.com>
2023-11-23 11:53:21 +0200

    f

--- middleware/menu_item.middleware.js
+++ middleware/menu_item.middleware.js
@@ -4,10 +4,14 @@ const {catchHandler} = require("../utils/handler.utils");
 const {resolveError} = require("../utils/translations.utils");
 const {Logger} = require("./log.middleware");
 
-const checkMenuItemOwner = message => (req, res, next) => {
+const checkMenuItemOwner = () => (req, res, next) => {
     const logger = new Logger(req);
 
-    dbRequest(logger.addQueryDB(QUERY.MENU_ITEM.CHECK_OWNERSHIP_SELECT_BY_CUSTOMER_ID_AND_MENU_ITEM_ID(req.customer.id, req.body.menuItemId)))
+    dbRequest(
+        logger.addQueryDB(
+            QUERY.MENU_ITEM.CHECK_OWNERSHIP_SELECT_BY_CUSTOMER_ID_AND_MENU_ITEM_ID(req.customer.id, req.body.id)
+        )
+    )
         .then(res => {
             if (!res.length) {
                 throw new Error(resolveError("MENU_ITEM.ONLY_OWNER_CAN", req));
