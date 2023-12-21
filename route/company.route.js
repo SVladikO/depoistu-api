e779388a3ce4223d543ed649954e4d3f6c5d4cc4
Vlad Serhiychuk <SVladikO@mail.ru>
2023-12-21 19:07:37 +0200

    f

--- route/company.route.js
+++ route/company.route.js
@@ -135,13 +135,13 @@ const routes = {
                 bodyValidation: true,
                 requestBody: {
                     name: VALIDATION.COMPANY.name.type,
+                    phone1: VALIDATION.COMPANY.phone1.type,
+                    phone2: VALIDATION.COMPANY.phone2.type,
+                    phone3: VALIDATION.COMPANY.phone3.type,
                     cityId: VALIDATION.COMPANY.cityId.type,
                     street: VALIDATION.COMPANY.street.type,
                     latitude: VALIDATION.COMPANY.latitude.type,
                     longitude: VALIDATION.COMPANY.longitude.type,
-                    phone1: VALIDATION.COMPANY.phone1.type,
-                    phone2: VALIDATION.COMPANY.phone2.type,
-                    phone3: VALIDATION.COMPANY.phone3.type,
                     schedule: VALIDATION.COMPANY.schedule.type,
                 },
             },
@@ -209,7 +209,7 @@ const routes = {
             details: {
                 ...PERMISSION(['4. Check ownership.']),
                 requestBody: {
-                    companyId: VALIDATION.COMPANY.id.type
+                    id: VALIDATION.COMPANY.id.type
                 }
             },
             description: DESCRIPTION.COMPANY.DELETE,
